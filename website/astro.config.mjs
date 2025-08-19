f1db3590a092dec4f9743649962fdb9297c2ad77
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2025-08-19 20:26:13 +0200

    feat: use auto import for Kbd component

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 
+import AutoImport from 'astro-auto-import'
+
 import starlightLinksValidator from 'starlight-links-validator'
 import starlightLlmsTxt from 'starlight-llms-txt'
 import starlightImageZoom from 'starlight-image-zoom'
@@ -13,60 +15,66 @@ import starlightChangelogs, {
 
 import catppuccin from '@catppuccin/starlight'
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astroeditor.danny.is',
-  integrations: [
-    starlight({
-      title: 'Astro Editor',
-      logo: {
-        src: './public/icon.png',
+  integrations: [AutoImport({
+    imports: [
+      {
+        'starlight-kbd/components': ['Kbd'],
       },
-      favicon: '/icon.png',
-      social: [
-        {
-          icon: 'github',
-          label: 'GitHub',
-          href: 'https://github.com/dannysmith/astro-editor',
-        },
-      ],
-      customCss: ['./src/styles/custom.css'],
-      lastUpdated: true,
-      sidebar: [
-        {
-          label: 'Guides',
-          autogenerate: { directory: 'guides' },
-        },
-        {
-          label: 'Sidebar',
-          autogenerate: { directory: 'sidebar' },
-        },
-        {
-          label: 'Recent versions',
-          items: [
-            ...makeChangelogsSidebarLinks([
-              {
-                type: 'recent',
-                base: 'changelog',
-                count: 7,
-              },
-            ]),
-          ],
-        },
-      ],
-      plugins: [
-        starlightLinksValidator(),
-        starlightLlmsTxt(),
-        starlightImageZoom(),
-        starlightGitHubAlerts(),
-        starlightKbd({
-          types: [{ id: 'mac', label: 'macOS', default: true }],
-          globalPicker: false,
-        }),
-        starlightChangelogs(),
-        catppuccin(),
-      ],
-      credits: true,
-    }),
-  ],
+    ],
+  }), starlight({
+    title: 'Astro Editor',
+    logo: {
+      src: './public/icon.png',
+    },
+    favicon: '/icon.png',
+    social: [
+      {
+        icon: 'github',
+        label: 'GitHub',
+        href: 'https://github.com/dannysmith/astro-editor',
+      },
+    ],
+    customCss: ['./src/styles/custom.css'],
+    lastUpdated: true,
+    sidebar: [
+      {
+        label: 'Guides',
+        autogenerate: { directory: 'guides' },
+      },
+      {
+        label: 'Sidebar',
+        autogenerate: { directory: 'sidebar' },
+      },
+      {
+        label: 'Recent versions',
+        items: [
+          ...makeChangelogsSidebarLinks([
+            {
+              type: 'recent',
+              base: 'changelog',
+              count: 7,
+            },
+          ]),
+        ],
+      },
+    ],
+    plugins: [
+      starlightLinksValidator(),
+      starlightLlmsTxt(),
+      starlightImageZoom(),
+      starlightGitHubAlerts(),
+      starlightKbd({
+        types: [{ id: 'mac', label: 'macOS', default: true }],
+        globalPicker: false,
+      }),
+      starlightChangelogs(),
+      catppuccin(),
+    ],
+    credits: true,
+  }), mdx()],
 })
