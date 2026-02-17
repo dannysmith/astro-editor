95ffbb6f1b257a06499f9f658d7e1b8dee705de5
Danny Smith <hi@danny.is>
2026-02-17 05:56:06 +0000

    Add docs stubs

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -57,7 +57,81 @@ export default defineConfig({
       ],
       sidebar: [
         {
-          slug: 'getting-started',
+          label: 'Getting Started',
+          items: [
+            { slug: 'getting-started' },
+            { slug: 'getting-started/concepts' },
+            { slug: 'getting-started/example' },
+            { slug: 'getting-started/installation' },
+            { slug: 'getting-started/opening-a-project' },
+          ],
+        },
+        {
+          slug: 'philosophy',
+        },
+        {
+          label: 'The Editor',
+          items: [
+            { slug: 'editor/overview' },
+            { slug: 'editor/focus-mode' },
+            { slug: 'editor/typewriter-mode' },
+            { slug: 'editor/copyedit-modes' },
+            { slug: 'editor/auto-saving' },
+          ],
+        },
+        {
+          label: 'Editing Features',
+          items: [
+            { slug: 'editing/overview' },
+            { slug: 'editing/links' },
+            { slug: 'editing/content-linker' },
+            { slug: 'editing/images-and-files' },
+            { slug: 'editing/mdx-components' },
+          ],
+        },
+        {
+          label: 'File Management',
+          items: [
+            { slug: 'file-management/overview' },
+            { slug: 'file-management/drafts' },
+            { slug: 'file-management/filtering-and-sorting' },
+            { slug: 'file-management/ide-integration' },
+            { slug: 'file-management/creating-files' },
+          ],
+        },
+        {
+          label: 'Frontmatter & Schemas',
+          items: [
+            { slug: 'frontmatter/overview' },
+            { slug: 'frontmatter/basic-fields' },
+            { slug: 'frontmatter/image-fields' },
+            { slug: 'frontmatter/reference-fields' },
+            { slug: 'frontmatter/special-fields' },
+            { slug: 'frontmatter/required-fields' },
+            { slug: 'frontmatter/examples' },
+          ],
+        },
+        {
+          label: 'Preferences',
+          items: [
+            { slug: 'preferences/general' },
+            { slug: 'preferences/project' },
+            { slug: 'preferences/collections' },
+            { slug: 'preferences/advanced' },
+          ],
+        },
+        { slug: 'command-palette' },
+        { slug: 'troubleshooting' },
+        {
+          label: 'Reference',
+          collapsed: true,
+          items: [
+            { slug: 'reference/keyboard-shortcuts' },
+            { slug: 'reference/special-fields' },
+            { slug: 'reference/overrides' },
+            { slug: 'reference/advanced-preferences' },
+            { slug: 'reference/yaml' },
+          ],
         },
         {
           label: 'Releases',
